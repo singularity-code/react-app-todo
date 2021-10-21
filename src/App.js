532e5d7535dde7c02cb90783b19be501c205dde8
chris.lee <Savier!4349>
2021-10-21 14:54:17 +1100

    Base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
   return (
     <div>
-      TO-DO Application
+      <TodoTemplate>Application</TodoTemplate>
     </div>
   )
 }
---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,14 @@
+import './TodoTemplate.scss';
+
+import React from 'react'
+
+const TodoTemplate = ({ children }) => {
+    return (
+        <div className="TodoTemplate">
+            <div className="app-title">TODO LIST</div>
+            <div className="content">{children}</div>
+        </div>
+    )
+}
+
+export default TodoTemplate;
