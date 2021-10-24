071a7e66ead24998cd285024a17ac1a01e589ba8
D.L <neoxrock@gmail.com>
2021-10-24 20:30:29 +1100

    Insert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useCallback } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
@@ -14,16 +14,29 @@ const App = () => {
       id: 2,
       text: 'Do next',
       checked: false,
-    }
-  ])
+    },
+  ]);
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
   return (
     <div>
       <TodoTemplate>
-        <TodoInsert></TodoInsert>
-        <TodoList todos = {todos}></TodoList>
+        <TodoInsert onInsert={onInsert}></TodoInsert>
+        <TodoList todos={todos}></TodoList>
       </TodoTemplate>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,12 +1,33 @@
 import './TodoInsert.scss';
 import React from 'react';
 import { MdAdd } from 'react-icons/md';
+import { useState, useCallback } from 'react';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      // Submit trigger refresh page
+      e.preventDefault();
+      onInsert(value);
+      setValue('');
+    },
+    [onInsert, value],
+  );
+
   return (
     <div>
-      <form className="TodoInsert">
-        <input placeholder="Insert new TODO Item"></input>
+      <form className="TodoInsert" onSubmit={onSubmit}>
+        <input
+          placeholder="Insert new TODO Item"
+          value={value}
+          onChange={onChange}
+        ></input>
         <button type="submit">
           <MdAdd></MdAdd>
         </button>
