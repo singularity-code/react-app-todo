bc8822368bbf61f0c8c0c3af79405a21a895a5f0
D.L <neoxrock@gmail.com>
2021-10-29 20:55:50 +1100

    List Optimization

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,20 +7,25 @@ import './TodoListItem.scss';
 import cn from 'classnames';
 import React from 'react';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? (
-          <MdCheckBox></MdCheckBox>
-        ) : (
-          <MdCheckBoxOutlineBlank></MdCheckBoxOutlineBlank>
-        )}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline></MdRemoveCircleOutline>
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? (
+            <MdCheckBox></MdCheckBox>
+          ) : (
+            <MdCheckBoxOutlineBlank></MdCheckBoxOutlineBlank>
+          )}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline></MdRemoveCircleOutline>
+        </div>
       </div>
     </div>
   );
