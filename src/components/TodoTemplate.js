fbe79e1073237848475527e4327cdfcc1b4e6a13
chris.lee@myhomevault.com.au <chris.lee@myhomevault.com.au>
2022-10-18 14:04:54 +1100

    update

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -5,7 +5,7 @@ import React from 'react';
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <p>* Sample App to demo component optimization and SCSS</p>
+      <p>* Sample Application to demo component optimization and SCSS</p>
       <div className="app-title">TODO LIST</div>
       <div className="content">{children}</div>
       <p>DL Project</p>
